243a6166fd8f4ff35fcc063fde5d625b3aab5887
gozde-apkk <gozdeapak.ga.1@gmail.com>
2024-03-10 12:56:14 +0300

    registration and unit of work class created in persistance layer

---
+++ Infastructure/WebAPI.Persistance/Registration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Persistance.Context;
+using WebAPI.Persistance.Repositories;
+using WepAPI.Application.Interfaces.Repositories;
+using WepAPI.Application.Interfaces.UnitOfWorks;
+
+namespace WebAPI.Persistance
+{
+    public static class Registration
+    {
+        public static void AddPersistance(this IServiceCollection services, IConfiguration configuration) 
+        {
+            services.AddDbContext<AppDbContext>(opt =>
+            opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
+            services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
+
+
+            services.AddScoped<IUnitOfWork ,  IUnitOfWork>();
+        }
+
+    }
+}
---
+++ Infastructure/WebAPI.Persistance/UnitsOfWorks/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebAPI.Persistance.Context;
+using WebAPI.Persistance.Repositories;
+using WepAPI.Application.Interfaces.Repositories;
+using WepAPI.Application.Interfaces.UnitOfWorks;
+
+namespace WebAPI.Persistance.UnitsOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext dbContext;
+
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public async ValueTask DisposeAsync() => await dbContext.DisposeAsync();
+
+
+        public int Save() => dbContext.SaveChanges();
+        public async Task<int> SaveAsync() => await dbContext.SaveChangesAsync();
+        IReadRepository<T> IUnitOfWork.GetReadRepository<T>() => new ReadRepository<T>(dbContext);
+        IWriteRepository<T> IUnitOfWork.GetWriteRepository<T>() => new WriteRepository<T>(dbContext);
+    }
+}
